6c2d7130569f52e50fadf326ed2db8da9dd296f6
Billy.Su <g4691821@gmail.com>
2016-10-05 15:37:04 +0800

    add cpu run

--- workload/workload.c
+++ workload/workload.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <signal.h>
 
-
+int do_cpu_run(int cpu_load);
 
 int main(int argc, char** argv){
-        int i;
+        int i,pid,children=0;
         int cpu_load,mem_load,io_load;
         for(i=1; i < argc; i++){
                 char* arg = argv[i];
@@ -21,5 +24,52 @@ int main(int argc, char** argv){
         printf("CPU load %d\n",cpu_load);
         printf("Memory load %d\n",mem_load);
         printf("IO load %d\n",io_load);
+        if( cpu_load > 0 ){
+                switch(pid = fork()){
+                  case 0:
+                        alarm(5);
+                        exit( do_cpu_run(cpu_load) );
+                  case -1:
+                        printf("fork process error while adding cpu load\n");
+                  default:
+                        printf("CPU worker %d forked\n",pid);
+                        ++children;
+                }
+        }
+        if( mem_load > 0 ){
+        
+        }
+        if( io_load > 0 ){
+        
+        }
+        while( children ){
+                int status, ret;
+                if( (pid = wait(&status)) > 0 ){
+                        if(WIFEXITED(status)){
+                                if((ret = WEXITSTATUS(status)) ==0){
+                                        printf("Worker %d exit normally\n",pid);
+                                }else{
+                                        printf("Worker %d exit return error %d\n",pid,ret);
+                                }
+                        }else if (WIFSIGNALED(status)){
+                                if((ret = WTERMSIG(status)) == SIGALRM){
+                                        printf("Worker %d signalled normally\n",pid);
+                                }else{
+                                        printf("Worker %d got signal %d\n",pid,ret);
+                                }
+                        }
+                        --children;
+                }else{
+                        printf("ERROR wait for worker\n");
+                }
+        }
+        return 0;
+}
+
+int do_cpu_run(int cpu_load){
+        while(1){
+          sqrt(rand());
+        }
+
         return 0;
 }
