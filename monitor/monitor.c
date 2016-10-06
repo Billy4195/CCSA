8e298c89432978cb7fba8193ff4c9832934162f9
Billy.Su <g4691821@gmail.com>
2016-10-06 11:29:37 +0800

    add memory usage

--- monitor/monitor.c
+++ monitor/monitor.c
@@ -3,9 +3,10 @@
 
 int main(){
         FILE* fp;
-        char cpu_name[20];
+        char cpu_name[20],buf[50],unit[5];
         long pre[8],aft[8];
         long Total,PreTotal,totald,idled;
+        long num,mem_tol,mem_free,mem_buf,mem_cache,mem_used;
         double cpu_use;
         fp = fopen("/proc/stat","r");
         fscanf(fp,"%s %ld %ld %ld %ld %ld %ld %ld %ld",cpu_name,&pre[0],&pre[1],
@@ -24,6 +25,22 @@ int main(){
         totald = Total - PreTotal;
         idled = (aft[3]+aft[4]) - (pre[3]+pre[4]);
         cpu_use = (totald - idled)/(double)totald*100;
-        printf("CPU useage : %lf\n",cpu_use);
+        printf("CPU useage : %lf %%\n",cpu_use);
+
+        fp = fopen("/proc/meminfo","r");
+        fscanf(fp,"%s %ld %s",buf,&num,unit);
+        mem_tol = num;
+        fscanf(fp,"%s %ld %s",buf,&num,unit);
+        mem_free = num;
+        fscanf(fp,"%s %ld %s",buf,&num,unit);
+        fscanf(fp,"%s %ld %s",buf,&num,unit);
+        mem_buf = num;
+        fscanf(fp,"%s %ld %s",buf,&num,unit);
+        mem_cache = num;
+        fclose(fp);
+
+        mem_used = mem_tol - mem_free - mem_buf - mem_cache;
+        printf("Memory Usage: %ld/%ld MB (%lf %%)\n",mem_used/1024,mem_tol/1024, (double)mem_used/mem_tol*100);
+
         return 0;
 }
